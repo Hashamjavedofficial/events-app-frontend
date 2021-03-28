7467cd1424ad17bfeaa35a83fbc9b8adb2f1f97c
Hasham Javed <hasham.hasham1@gmail.com>
2021-03-28 12:19:47 +0500

    application setup with material ui tailwindcss and redux

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const reducers = combineReducers({});
+
+export const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
---
+++ src/utils/theme.js
@@ -0,0 +1,27 @@
+import {createMuiTheme} from "@material-ui/core";
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#252683",
+        },
+        secondary: {
+            main: "#fcd124",
+        },
+    },
+    // typography: {
+    //     fontFamily: ["Nunito", "sans-serif"],
+    // },
+    overrides: {
+        MuiButton: {
+            root: {
+                textTransform: "none",
+            },
+        },
+    },
+    props: {
+        MuiButton: {
+            disableRipple: true,
+        },
+    },
+});
